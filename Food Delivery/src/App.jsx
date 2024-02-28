131af7e4b8ce05cafb49124dd129b1d2c0469409
Saurabh <kumarsaurabh8286@gmail.com>
2024-02-28 11:47:38 +0530

    first

---
+++ Food Delivery/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './Components/Header'
+function App() {
+
+  return (
+    <>
+      <div>
+        <Header></Header>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ Food Delivery/src/Components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <div>
+        <p>Yammi<span>Food</span></p>
+      </div>
+      <div>
+        <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Offers</li>
+            <li>Order Now</li>
+            <li>Sign </li>
+        </ul>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Header;
